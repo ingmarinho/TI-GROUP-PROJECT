449ee200f7e9b86a175cc3fcb344ef3070285069
IanRietveld <74311741+IanRietveld@users.noreply.github.com>
2021-04-08 12:32:29 +0200

    Updated commentary.

--- Sprint 3/src/nice effort/hardware.h
+++ Sprint 3/src/nice effort/hardware.h
@@ -6,15 +6,15 @@
 #include <vector>
 
 #define TRIG1 16    // GPIO 15 | Output distance sensor (TRIG1) (BOAT DETECTION FRONT)
-#define ECHO1 15    // GPIO 14 | Input distance sensor (ECHO1) (BOAT DETECTION FRONT)
+#define ECHO1 15    // GPIO 14 | Input distance sensor  (ECHO1) (BOAT DETECTION FRONT)
 #define TRIG2 1     // GPIO 18 | Output distance sensor (TRIG2) (BOAT DETECTION BACK)
-#define ECHO2 4     // GPIO 23 | Input distance sensor (ECHO2) (BOAT DETECTION BACK)
+#define ECHO2 4     // GPIO 23 | Input distance sensor  (ECHO2) (BOAT DETECTION BACK)
 
-#define LED1 11
-#define LED2 27
+#define LED1 11     // GPIO 7  | Output waarschuwings LED links  (LED1)
+#define LED2 27     // GPIO 16 | Output waarschuwings LED rechts (LED2)
 
-#define SERV1 5     // GPIO 25 | Output servo (SERV1) (BRIDGE)
-#define SERV2 31    // GPIO 26 | Output servo (SERV2) (BARRIER 1)
+#define SERV1 5     // GPIO 24 | Output servo (SERV1) (BRIDGE)
+#define SERV2 31    // GPIO 1  | Output servo (SERV2) (BARRIER 1)
 #define SERV3 26    // GPIO 12 | Output servo (SERV3) (BARRIER 2)
 
 #define CLCK 10     // GPIO 8  | Output led strip (CLCK) (BRIDGE LIGHT)
