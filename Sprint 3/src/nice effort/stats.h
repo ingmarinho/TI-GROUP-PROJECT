a5040c26aeb3a6338d6dd72bd071f724cf0949b6
Joris Santen <joris.j.vansanten@student.hu.nl>
2021-04-08 12:00:58 +0200

    stats.h kan nu bruikbaar zijn.
    ik heb het iets of wat beter werkend gemaakt

--- Sprint 3/src/nice effort/stats.h
+++ Sprint 3/src/nice effort/stats.h
@@ -1,3 +1,8 @@
+#include <vector>
+#include <unistd.h>
+
+using namespace std;
+
 // DEZE CODE WAS BEDOELD OM VOOR STATISTIEKEN VAN DE BRUG TE GEBRUIKEN IN DE GUI DIE WE NIET HEBBEN KUNNEN REALISEREN.
 
 int counter_open_totaal = 200;
@@ -8,21 +13,24 @@ int counter_maand_totaal = 200;
 vector<int> counter_dagen_week = {25, 30, 29, 25, 33, 23, 35};
 vector<int> counter_afgelopen_dagen = {0, 6, 8, 7, 5, 9, 7, 5, 10, 6, 7, 3, 8, 9, 6, 7, 5, 8, 10, 6, 6, 5, 8, 4, 11, 7, 6, 8, 9, 4};
 
-cout << "Totaal is nu: " << counter_open_totaal << ", vandaag is nu: " << counter_afgelopen_dagen[0] << " deze week is nu: " << counter_week_totaal << " deze maand is nu: " << counter_maand_totaal << endl;
-if (pointerday == 0)
-    cout << "het is nu maandag, totaal op maandagen: " << counter_dagen_week[0] << ", totaal op dinsdagen: " << counter_dagen_week[1] << ", totaal op woensdagen: " << counter_dagen_week[2] << ", totaal op donderdagen: " << counter_dagen_week[3] << ", totaal op vrijdagen: " << counter_dagen_week[4] << ", totaal op zaterdagen: " << counter_dagen_week[5] << ", totaal op zondagen: " << counter_dagen_week[6] << endl;
-if (pointerday == 1)
-    cout << "het is nu dinsdag, totaal op maandagen: " << counter_dagen_week[0] << ", totaal op dinsdagen: " << counter_dagen_week[1] << ", totaal op woensdagen: " << counter_dagen_week[2] << ", totaal op donderdagen: " << counter_dagen_week[3] << ", totaal op vrijdagen: " << counter_dagen_week[4] << ", totaal op zaterdagen: " << counter_dagen_week[5] << ", totaal op zondagen: " << counter_dagen_week[6] << endl;
-if (pointerday == 2)
-    cout << "het is nu woensdag, totaal op maandagen: " << counter_dagen_week[0] << ", totaal op dinsdagen: " << counter_dagen_week[1] << ", totaal op woensdagen: " << counter_dagen_week[2] << ", totaal op donderdagen: " << counter_dagen_week[3] << ", totaal op vrijdagen: " << counter_dagen_week[4] << ", totaal op zaterdagen: " << counter_dagen_week[5] << ", totaal op zondagen: " << counter_dagen_week[6] << endl;
-if (pointerday == 3)
-    cout << "het is nu donderdag, totaal op maandagen: " << counter_dagen_week[0] << ", totaal op dinsdagen: " << counter_dagen_week[1] << ", totaal op woensdagen: " << counter_dagen_week[2] << ", totaal op donderdagen: " << counter_dagen_week[3] << ", totaal op vrijdagen: " << counter_dagen_week[4] << ", totaal op zaterdagen: " << counter_dagen_week[5] << ", totaal op zondagen: " << counter_dagen_week[6] << endl;
-if (pointerday == 4)
-    cout << "het is nu vrijdag, totaal op maandagen: " << counter_dagen_week[0] << ", totaal op dinsdagen: " << counter_dagen_week[1] << ", totaal op woensdagen: " << counter_dagen_week[2] << ", totaal op donderdagen: " << counter_dagen_week[3] << ", totaal op vrijdagen: " << counter_dagen_week[4] << ", totaal op zaterdagen: " << counter_dagen_week[5] << ", totaal op zondagen: " << counter_dagen_week[6] << endl;
-if (pointerday == 5)
-    cout << "het is nu zaterdag, totaal op maandagen: " << counter_dagen_week[0] << ", totaal op dinsdagen: " << counter_dagen_week[1] << ", totaal op woensdagen: " << counter_dagen_week[2] << ", totaal op donderdagen: " << counter_dagen_week[3] << ", totaal op vrijdagen: " << counter_dagen_week[4] << ", totaal op zaterdagen: " << counter_dagen_week[5] << ", totaal op zondagen: " << counter_dagen_week[6] << endl;
-if (pointerday == 6)
-    cout << "het is nu zondag, totaal op maandagen: " << counter_dagen_week[0] << ", totaal op dinsdagen: " << counter_dagen_week[1] << ", totaal op woensdagen: " << counter_dagen_week[2] << ", totaal op donderdagen: " << counter_dagen_week[3] << ", totaal op vrijdagen: " << counter_dagen_week[4] << ", totaal op zaterdagen: " << counter_dagen_week[5] << ", totaal op zondagen: " << counter_dagen_week[6] << endl;
+void printstatus()
+{
+    cout << "Totaal is nu: " << counter_open_totaal << ", vandaag is nu: " << counter_afgelopen_dagen[0] << " deze week is nu: " << counter_week_totaal << " deze maand is nu: " << counter_maand_totaal << endl;
+    if (pointerday == 0)
+        cout << "het is nu maandag, totaal op maandagen: " << counter_dagen_week[0] << ", totaal op dinsdagen: " << counter_dagen_week[1] << ", totaal op woensdagen: " << counter_dagen_week[2] << ", totaal op donderdagen: " << counter_dagen_week[3] << ", totaal op vrijdagen: " << counter_dagen_week[4] << ", totaal op zaterdagen: " << counter_dagen_week[5] << ", totaal op zondagen: " << counter_dagen_week[6] << endl;
+    if (pointerday == 1)
+        cout << "het is nu dinsdag, totaal op maandagen: " << counter_dagen_week[0] << ", totaal op dinsdagen: " << counter_dagen_week[1] << ", totaal op woensdagen: " << counter_dagen_week[2] << ", totaal op donderdagen: " << counter_dagen_week[3] << ", totaal op vrijdagen: " << counter_dagen_week[4] << ", totaal op zaterdagen: " << counter_dagen_week[5] << ", totaal op zondagen: " << counter_dagen_week[6] << endl;
+    if (pointerday == 2)
+        cout << "het is nu woensdag, totaal op maandagen: " << counter_dagen_week[0] << ", totaal op dinsdagen: " << counter_dagen_week[1] << ", totaal op woensdagen: " << counter_dagen_week[2] << ", totaal op donderdagen: " << counter_dagen_week[3] << ", totaal op vrijdagen: " << counter_dagen_week[4] << ", totaal op zaterdagen: " << counter_dagen_week[5] << ", totaal op zondagen: " << counter_dagen_week[6] << endl;
+    if (pointerday == 3)
+        cout << "het is nu donderdag, totaal op maandagen: " << counter_dagen_week[0] << ", totaal op dinsdagen: " << counter_dagen_week[1] << ", totaal op woensdagen: " << counter_dagen_week[2] << ", totaal op donderdagen: " << counter_dagen_week[3] << ", totaal op vrijdagen: " << counter_dagen_week[4] << ", totaal op zaterdagen: " << counter_dagen_week[5] << ", totaal op zondagen: " << counter_dagen_week[6] << endl;
+    if (pointerday == 4)
+        cout << "het is nu vrijdag, totaal op maandagen: " << counter_dagen_week[0] << ", totaal op dinsdagen: " << counter_dagen_week[1] << ", totaal op woensdagen: " << counter_dagen_week[2] << ", totaal op donderdagen: " << counter_dagen_week[3] << ", totaal op vrijdagen: " << counter_dagen_week[4] << ", totaal op zaterdagen: " << counter_dagen_week[5] << ", totaal op zondagen: " << counter_dagen_week[6] << endl;
+    if (pointerday == 5)
+        cout << "het is nu zaterdag, totaal op maandagen: " << counter_dagen_week[0] << ", totaal op dinsdagen: " << counter_dagen_week[1] << ", totaal op woensdagen: " << counter_dagen_week[2] << ", totaal op donderdagen: " << counter_dagen_week[3] << ", totaal op vrijdagen: " << counter_dagen_week[4] << ", totaal op zaterdagen: " << counter_dagen_week[5] << ", totaal op zondagen: " << counter_dagen_week[6] << endl;
+    if (pointerday == 6)
+        cout << "het is nu zondag, totaal op maandagen: " << counter_dagen_week[0] << ", totaal op dinsdagen: " << counter_dagen_week[1] << ", totaal op woensdagen: " << counter_dagen_week[2] << ", totaal op donderdagen: " << counter_dagen_week[3] << ", totaal op vrijdagen: " << counter_dagen_week[4] << ", totaal op zaterdagen: " << counter_dagen_week[5] << ", totaal op zondagen: " << counter_dagen_week[6] << endl;
+}
 
 void countersadd()
 {
@@ -39,7 +47,7 @@ void countersupdate()
     counter_maand_totaal = 0;
     for (int i = 29; i > 0; i++)
     {
-        counter_afgelopen_dagen[i] = counter_afgelopen_dagen[i-1];
+        counter_afgelopen_dagen[i] = counter_afgelopen_dagen[i - 1];
         counter_maand_totaal += counter_afgelopen_dagen[i];
     }
     for (int i = 0; i < 7; i++)
