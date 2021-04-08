f26e65e001b455910a8c1e2411c1b5303da0465d
IanRietveld <74311741+IanRietveld@users.noreply.github.com>
2021-04-08 12:22:20 +0200

    Updated commentary.

--- Sprint 3/src/nice effort/hardware.cpp
+++ Sprint 3/src/nice effort/hardware.cpp
@@ -24,7 +24,7 @@ void setup()
     pinMode(BEEP, OUTPUT);
 }
 
-void pulse(const int &pin, const unsigned int &delay1, const unsigned int &wait_time) // code om voor een bepaalde tijd een led aan te doen
+void pulse(const int &pin, const unsigned int &delay1, const unsigned int &wait_time) // Code om voor een bepaalde tijd een led aan te doen.
 {
     digitalWrite(pin, HIGH);
     usleep(delay1);
@@ -32,17 +32,17 @@ void pulse(const int &pin, const unsigned int &delay1, const unsigned int &wait_
     usleep(wait_time);
 }
 
-void changeServoPosition(const int pin, const int position) // 0 to 100 (0 to 180°)
+void changeServoPosition(const int pin, const int position) // Servo position 0 to 100 (0 to 180°)
 {
-    int wait_time = 20; // basis reken waardes voor de formule
+    int wait_time = 20; // Basis waardes voor de rekenformule van de servo. 
     float step_size = 0.02f;
     float base_time = 0.4f;
-    unsigned int servo_pulse_time = (base_time + (position * step_size)) * 1000; // conversion to microseconds (usleep is in microseconds)
+    unsigned int servo_pulse_time = (base_time + (position * step_size)) * 1000; // Conversion to microseconds (usleep is in microseconds)
 
     pulse(pin, servo_pulse_time, wait_time); // stuur een servo aan op pin.
 }
 
-void activateLeds() // activeer 2 leds om aan te gaan, voor het waarschuw singaal.
+void activateLeds() // Activeer 2 leds om aan te gaan, voor het waarschuwing signaal.
 {
     digitalWrite(LED1, 1);
     digitalWrite(LED2, 1);
@@ -51,28 +51,28 @@ void activateLeds() // activeer 2 leds om aan te gaan, voor het waarschuw singaa
     digitalWrite(LED2, 0);
 }
 
-void send_bytes(std::vector<bool> bytes) // een rij van 4 boolians die in de ledstrip bepalen welke kleur de volgende lamp heeft.
+void send_bytes(std::vector<bool> bytes) // Een rij van 4 booleans die in de ledstrip bepalen welke kleur de LEDs hebben.
 {
     vector<bool> k = {};
 
-    for (unsigned int i = 0; i < bytes.size(); i++) // loop door de 4 gegeven waardes heen.
+    for (unsigned int i = 0; i < bytes.size(); i++) // Loop door de vier gegeven waardes heen.
     {
-        if (i == 0 && bytes[i]) // zorg er voor dat de leds niet te fel zijn als die aan moet.
+        if (i == 0 && bytes[i]) // Zorg er voor dat de leds niet te fel zijn als ze aan staan.
         {
             k = {1, 1, 1, 0, 0, 0, 1, 0};
         }
-        else if (i != 0 && bytes[i]) // zorg er voor dat een RGB kleur volledig word laten zien.
+        else if (i != 0 && bytes[i]) // Zorg er voor dat een RGB kleur volledig word laten zien.
         {
             k = {1, 1, 1, 1, 1, 1, 1, 1};
         }
-        else // stuur anders niks door
+        else // Stuur anders niks door.
         {
             k = {0, 0, 0, 0, 0, 0, 0, 0};
         }
 
-        for (unsigned int j = 0; j < k.size(); j++) // stuur bits ob basis van de hierboven gegeven waardes door.
+        for (unsigned int j = 0; j < k.size(); j++) // Stuur bits op basis van de hierboven gegeven waardes door.
         {
-            if (k[j]) // stuur een 1 of een 0
+            if (k[j]) // Stuur een 1 of een 0.
                 digitalWrite(INFO, HIGH);
             else
                 digitalWrite(INFO, LOW);
@@ -82,39 +82,39 @@ void send_bytes(std::vector<bool> bytes) // een rij van 4 boolians die in de led
     }
 }
 
-void activateBoatTrafficLight(bool color) // 0 = rood | 1 = groen // zeg welke kleur de ledstrip moet hebben.
+void activateBoatTrafficLight(bool color) // 0 = rood | 1 = groen // Zeg welke kleur de ledstrip moet hebben.
 {
 
-    send_bytes({0, 0, 0, 0}); // reset de ledstrip
-    for (unsigned int i = 0; i < 9; i++) // stuur informatie ter waarde van negen leds door, anders heeft de laaste led problemen.
+    send_bytes({0, 0, 0, 0}); // Reset de ledstrip
+    for (unsigned int i = 0; i < 9; i++) // Stuur informatie ter waarde van negen leds door, anders heeft de laaste led problemen.
     {
         if (!color)
-            send_bytes({1, 0, 0, 1}); // maak de led rood
+            send_bytes({1, 0, 0, 1}); // Maak de LEDs rood.
         else
-            send_bytes({1, 0, 1, 0}); // maak de led groen
+            send_bytes({1, 0, 1, 0}); // Maak de LEDs groen.
     }
 }
 
-void barrierSound() // maak een geluid voor de slagbomen.
+void barrierSound() // Geluid voor de slagbomen.
 {
     digitalWrite(BEEP, HIGH);
     delay(200);
     digitalWrite(BEEP, LOW);
 }
 
-int getCurrentDistance(const int trig_pin, const int echo_pin) // bepaal de afstand van een afstand sensor.
+int getCurrentDistance(const int trig_pin, const int echo_pin) // Functionaliteit van de afstandssensoren.
 {
-    digitalWrite(trig_pin, HIGH); // laat de sensor beginnen met meten.
+    digitalWrite(trig_pin, HIGH); // Laat de sensor beginnen met meten.
     delay(20);
     digitalWrite(trig_pin, LOW);
 
-    while (digitalRead(echo_pin) == LOW); // wacht tot dat de echo een hoge output heeft.
-    long int startTime = micros(); // meet de tijd
-    while (digitalRead(echo_pin) == HIGH); // wacht tot dat de echo een lage output heeft.
+    while (digitalRead(echo_pin) == LOW); // Wacht tot dat de echo een hoge output heeft.
+    long int startTime = micros(); // Meet de tijd.
+    while (digitalRead(echo_pin) == HIGH); // Wacht tot dat de echo een lage output heeft.
 
-    long int travelTime = micros() - startTime; // bereken de tijd tussen het begin van de hoge output en het einde.
+    long int travelTime = micros() - startTime; // Bereken de tijd tussen het begin van de hoge output en het einde.
 
-    int distance = travelTime / 58; // bereken de afstand in hele centimeters
+    int distance = travelTime / 58; // Bereken de afstand in hele centimeters.
 
     return distance; // geef de afstand terug.
 }
